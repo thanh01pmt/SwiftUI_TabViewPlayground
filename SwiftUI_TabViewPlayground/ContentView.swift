addb85f2f3294b3192beb2935feb0ebc0f2b6fce
Tony Pham <thanh01.pmt@gmail.com>
2025-05-24 13:39:49 +0700

    Initial Commit

---
+++ SwiftUI_TabViewPlayground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_TabViewPlayground
+//
+//  Created by Tony Pham on 24/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_TabViewPlayground/SwiftUI_TabViewPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TabViewPlaygroundApp.swift
+//  SwiftUI_TabViewPlayground
+//
+//  Created by Tony Pham on 24/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TabViewPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
